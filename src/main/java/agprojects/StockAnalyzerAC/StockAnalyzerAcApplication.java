4af21322e4ee6539562c260af5f89ae1e78289a7
Alexander Georgiev <wishiper@gmail.com>
2020-12-19 15:20:45 +0200

    Spring boot init project

---
+++ src/main/java/agprojects/StockAnalyzerAC/StockAnalyzerAcApplication.java
@@ -0,0 +1,14 @@
+package agprojects.StockAnalyzerAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyzerAcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyzerAcApplication.class, args);
+		System.out.println("StockAnalyzer started successfully");
+	}
+
+}
---
+++ src/test/java/agprojects/StockAnalyzerAC/StockAnalyzerAcApplicationTests.java
@@ -0,0 +1,13 @@
+package agprojects.StockAnalyzerAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyzerAcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
